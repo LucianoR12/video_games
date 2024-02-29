1faa43d5bab7aae2c92256446e39ab799a042fdf
Luciano Rubino <rbn.lcn@gmail.com>
2024-02-29 10:48:36 +0000

    first commit

---
+++ video_games.py
@@ -0,0 +1,482 @@
+# # # # # # # # # # # #
+#   LIBRARIES         #
+# # # # # # # # # # # #
+
+import pandas as pd
+import sys
+from io import StringIO
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+
+# # # # # # # # # # # #
+#   OUTPUT FILES      #
+# # # # # # # # # # # #
+
+# get the file path
+def get_file_path(file_name, extension):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_directory, f"{os.path.splitext(file_name)[0]}.{extension}")
+    return file_path
+
+def get_log_file_path(file_name):
+    return get_file_path(file_name, "log")
+
+def get_txt_file_path(file_name):
+    return get_file_path(file_name, "txt")
+
+def get_png_file_path(file_name):
+    return get_file_path(file_name, "png")
+
+def get_xlsx_file_path(file_name):
+    return get_file_path(file_name, "xlsx")
+
+def get_csv_file_path(file_name):
+    return get_file_path(file_name, "csv")
+
+# setup logging to save logs to a file
+def setup_logging(log_file):
+    logging.basicConfig(level=logging.INFO, filemode='w', filename=log_file)
+    logging.getLogger().setLevel(logging.INFO)
+
+# read the csv file, extract the name and create a txt output file
+def start_process_csv_file(file_name):
+    logging.info(f"'start process csv file' {file_name} started.")
+    df = pd.read_csv(file_name)
+    output_file = f"{os.path.splitext(file_name)[0]}.txt"
+    output_buffer = StringIO()
+    sys.stdout = output_buffer
+    logging.info("'start process csv file' completed.")
+    return df, output_file, output_buffer
+
+# save the analysis results to the txt file
+def end_process_csv_file(output_file, output_buffer):
+    logging.info("'end process csv file' started.")
+    sys.stdout = sys.__stdout__
+    with open(output_file, "w") as output_file:
+        output_file.write(output_buffer.getvalue())
+        logging.info("'end process csv file' completed.")
+
+# # # # # # # # # # # #
+#   CHECKS            #
+# # # # # # # # # # # #
+
+# check if the excel file exists
+def check_csv_file(file_name):
+    logging.info("'check csv file' started.")
+    if not os.path.isfile(file_name):
+        logging.error(f"Error: csv file '{file_name}' not found.")
+        sys.exit(1)
+    else:
+        logging.info("'check csv file' completed.")
+
+# check if the columns exist
+def columns_validation(df):
+    logging.info("'columns validation' started.")
+    required_columns = ['Title', 'Release Date', 'Developer', 'Publisher', 'Genres', 'Product Rating', 'User Score', 'User Ratings Count', 'Platforms Info']
+    missing_columns = [column for column in required_columns if column not in df.columns]
+    if missing_columns:
+        missing_columns_str = ', '.join(missing_columns)
+        logging.warning(f"Missing required column(s): {missing_columns_str}")
+        sys.exit(1)
+    else:
+        logging.info("'columns validation' completed.")
+
+# # # # # # # # # # # #
+#   INFO              #
+# # # # # # # # # # # #
+
+# print the info of the excel file
+def print_info(df):
+    logging.info("'print info' started.")
+    df.info()
+    logging.info("'print info' completed.")
+
+# print a description of the excel file
+def print_description(df):
+    logging.info("'print description' started.")
+    print(df.describe())
+    logging.info("'print description' completed.")
+
+# print the first 5 rows of the excel file
+def print_first_row(df):
+    logging.info("'print first row' started.")
+    print(df.head())
+    logging.info("'print first row' completed.")
+
+# # # # # # # # # # # #
+#   CLEANING          #
+# # # # # # # # # # # #
+
+# removes columns from the dataframe
+def remove_columns(df):
+    logging.info("'remove columns' started.")
+    df.drop(columns=['User Score', 'User Ratings Count', 'Platforms Info'], inplace=True)
+    logging.info("'remove columns' completed.")
+
+# rename column
+def rename_column(df):
+    logging.info("'rename column' started.")
+    df.rename(columns={'Product Rating' : 'Age Rating', 'Genres' : 'Genre'}, inplace=True)
+    logging.info("'rename column' completed.")
+
+# removes rows from the dataframe
+def remove_rows(df):
+    logging.info("'remove rows' started.")
+    df = df.drop([0], inplace=True)
+    logging.info("'remove rows' completed.")
+
+# removes duplicates from the dataframe
+def remove_duplicates(df):
+    logging.info("'remove duplicates' started.")
+    df = df.drop_duplicates(inplace=True)
+    logging.info("'remove duplicates' completed.")
+
+# reset index after removing rows
+def reset_index(df):
+    logging.info("'reset index' started.")
+    df = df.reset_index(drop=True, inplace=True)
+    logging.info("'reset index' completed.")
+
+# check missing values
+def check_missing_values(df):
+    logging.info("'check missing values' started.")
+    missing_values = df.isnull().sum()
+    print('Missing values:\n', missing_values)
+    
+    if missing_values.any():
+        df.dropna(inplace=True)
+        logging.info("Rows with missing values dropped.")
+    
+    # df.fillna(value=fill_value, inplace=True)
+    
+    logging.info("'check missing values' completed.")
+    return missing_values
+
+# clean up the "Age Rating" column
+def clean_age_rating(df):
+    logging.info("'clean age rating' started.")
+    rating_mapping = {
+        'Rated T For Teen': 'Teen',
+        'Rated E For Everyone': 'Everyone',
+        'Rated M For Mature': 'Mature',
+        'Rated E +10 For Everyone +10': 'Everyone +10',
+        'Rated RP For Rate Pending': 'Rate Pending',
+        'Rated AO For Adults Only': 'Adults Only'
+    }
+    df['Age Rating'] = df['Age Rating'].map(rating_mapping)
+    logging.info("'clean age rating' completed.")
+
+# # # # # # # # # # # #
+#   TITLE             #
+# # # # # # # # # # # #
+
+# the sum of all the games
+def total_games(df):
+    logging.info("'total games' started.")
+    total_games = df["Title"].sum()
+    print(f"The total games made is: {total_games:.0f}")
+    logging.info("'total games' completed.")
+
+# # # # # # # # # # # #
+#   RELEASE DATE      #
+# # # # # # # # # # # #
+
+# release date bar chart
+def release_date_bar_chart(df, file_name):
+    logging.info("'release date bar chart' started.")
+
+    df['Release Year'] = pd.to_datetime(df['Release Date']).dt.year
+    release_year_count = df['Release Year'].value_counts().sort_index()
+
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    bar_colors = sns.color_palette("viridis", len(release_year_count))
+    bars = ax.bar(release_year_count.index.astype(int), release_year_count, color=bar_colors)
+
+    plt.xticks(release_year_count.index.astype(int), rotation=45)
+
+    ax.set_title("Games released by year", fontsize=16, color='white')
+    ax.set_xlabel("Year", fontsize=12, color='white')
+    ax.set_ylabel("Games", fontsize=12, color='white')
+
+    ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    png_file_name = f"{os.path.splitext(file_name)[0]}_release_date.png"
+    plt.savefig(png_file_name, bbox_inches='tight', dpi=300)
+    logging.info("'release date bar chart' completed.")
+    plt.show()
+
+# # # # # # # # # # # #
+#   DEVELOPER         #
+# # # # # # # # # # # #
+
+# counts all the games by developer
+def count_developer(df):
+    logging.info("'count developer' started.")
+    count_developer = df["Developer"].value_counts()
+    count_developer.index.name = None
+    print("Games by developer:")
+    print(count_developer.to_string(name=False))
+    logging.info("'count developer' completed.")
+
+# highlight the most common payment method
+def most_common_developer(df):
+    logging.info("'most common developer' started.")
+    most_common_developer = df["Developer"].mode().iloc[0]
+    print(f"The most common developer is: {most_common_developer}")
+    logging.info("'most common developer' completed.")
+
+# highlight the least common payment method
+def least_common_developer(df):
+    logging.info("'least common developer' started.")
+    least_common_developer = min(df["Developer"].unique(), key=df["Developer"].tolist().count)
+    print(f"The least common developer is: {least_common_developer}")
+    logging.info("'least common developer' completed.")
+
+# developer bar chart
+def developer_bar_chart(df, file_name):
+    logging.info("'developer bar chart' started.")
+
+    developer_count_top = df['Developer'].value_counts().head(10)
+    developer_count_bottom = df['Developer'].value_counts().tail(10)
+
+    plt.style.use('dark_background')
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+
+    bar_colors_top = sns.color_palette("viridis", len(developer_count_top))
+    bars_top = ax1.bar(developer_count_top.index, developer_count_top, color=bar_colors_top)
+
+    ax1.set_title("Top 10 Developers", fontsize=16, color='white')
+    ax1.set_xlabel("Developer", fontsize=12, color='white')
+    ax1.set_ylabel("Games", fontsize=12, color='white')
+
+    ax1.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax1.tick_params(axis='x', colors='white')
+    ax1.tick_params(axis='y', colors='white')
+
+    for bar in bars_top:
+        yval = bar.get_height()
+        ax1.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    bar_colors_bottom = sns.color_palette("viridis", len(developer_count_bottom))
+    bars_bottom = ax2.bar(developer_count_bottom.index, developer_count_bottom, color=bar_colors_bottom)
+
+    ax2.set_title("Bottom 10 Developers", fontsize=16, color='white')
+    ax2.set_xlabel("Developer", fontsize=12, color='white')
+    ax2.set_ylabel("Games", fontsize=12, color='white')
+
+    ax2.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax2.tick_params(axis='x', colors='white')
+    ax2.tick_params(axis='y', colors='white')
+
+    for bar in bars_bottom:
+        yval = bar.get_height()
+        ax2.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    plt.subplots_adjust(hspace=0.5)
+
+    png_file_name = f"{os.path.splitext(file_name)[0]}_developer.png"
+    plt.savefig(png_file_name, bbox_inches='tight', dpi=300)
+    logging.info("'developer bar chart' completed.")
+    plt.show()
+
+# # # # # # # # # # # #
+#   PUBLISHER         #
+# # # # # # # # # # # #
+
+# counts all the games by publisher
+def count_publisher(df):
+    logging.info("'count publisher' started.")
+    count_publisher = df["Publisher"].value_counts()
+    count_publisher.index.name = None
+    print("Games by publisher:")
+    print(count_publisher.to_string(name=False))
+    logging.info("'count publisher' completed.")
+
+# highlight the most common payment method
+def most_common_publisher(df):
+    logging.info("'most common publisher' started.")
+    most_common_publisher = df["Publisher"].mode().iloc[0]
+    print(f"The most common publisher is: {most_common_publisher}")
+    logging.info("'most common publisher' completed.")
+
+# highlight the least common payment method
+def least_common_publisher(df):
+    logging.info("'least common publisher' started.")
+    least_common_publisher = min(df["Publisher"].unique(), key=df["Publisher"].tolist().count)
+    print(f"The least common publisher is: {least_common_publisher}")
+    logging.info("'least common publisher' completed.")
+
+# publisher bar chart
+def publisher_bar_chart(df, file_name):
+    logging.info("'publisher bar chart' started.")
+
+    top_publisher_count = df['Publisher'].value_counts().head(10)
+    bottom_publisher_count = df['Publisher'].value_counts().tail(10)
+
+    plt.style.use('dark_background')
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+
+    bar_colors_top = sns.color_palette("viridis", len(top_publisher_count))
+    bars_top = ax1.bar(top_publisher_count.index, top_publisher_count, color=bar_colors_top)
+
+    ax1.set_title("Top 10 Publishers", fontsize=16, color='white')
+    ax1.set_xlabel("Publisher", fontsize=12, color='white')
+    ax1.set_ylabel("Games", fontsize=12, color='white')
+
+    ax1.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax1.tick_params(axis='x', colors='white')
+    ax1.tick_params(axis='y', colors='white')
+
+    for bar in bars_top:
+        yval = bar.get_height()
+        ax1.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    bar_colors_bottom = sns.color_palette("viridis", len(bottom_publisher_count))
+    bars_bottom = ax2.bar(bottom_publisher_count.index, bottom_publisher_count, color=bar_colors_bottom)
+
+    ax2.set_title("Bottom 10 Publishers", fontsize=16, color='white')
+    ax2.set_xlabel("Publisher", fontsize=12, color='white')
+    ax2.set_ylabel("Games", fontsize=12, color='white')
+
+    ax2.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax2.tick_params(axis='x', colors='white')
+    ax2.tick_params(axis='y', colors='white')
+
+    for bar in bars_bottom:
+        yval = bar.get_height()
+        ax2.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    plt.subplots_adjust(hspace=0.5)
+
+    png_file_name = f"{os.path.splitext(file_name)[0]}_publisher_chart.png"
+    plt.savefig(png_file_name, bbox_inches='tight', dpi=300)
+    logging.info("'publisher bar chart' completed.")
+    plt.show()
+
+# # # # # # # # # # # #
+#   GENRE             #
+# # # # # # # # # # # #
+
+# counts all the games by publisher
+def count_genre(df):
+    logging.info("'count genre' started.")
+    count_genre = df["Genre"].value_counts()
+    count_genre.index.name = None
+    print("Games by genre:")
+    print(count_genre.to_string(name=False))
+    logging.info("'count genre' completed.")
+
+# highlight the most common payment method
+def most_common_genre(df):
+    logging.info("'most common genre' started.")
+    most_common_genre = df["Genre"].mode().iloc[0]
+    print(f"The most common genre is: {most_common_genre}")
+    logging.info("'most common genre' completed.")
+
+# highlight the least common payment method
+def least_common_genre(df):
+    logging.info("'least common genre' started.")
+    least_common_genre = min(df["Genre"].unique(), key=df["Genre"].tolist().count)
+    print(f"The least common genre is: {least_common_genre}")
+    logging.info("'least common genre' completed.")
+
+# genre bar chart
+def genre_bar_chart(df, file_name):
+    logging.info("'genre bar chart' started.")
+
+    genre_count = df['Genre'].value_counts().sort_values(ascending=False)
+
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    bar_colors = sns.color_palette("viridis", len(genre_count))
+    bars = ax.bar(genre_count.index, genre_count, color=bar_colors)
+
+    plt.xticks(rotation=90, ha='right')
+
+    ax.set_title("Games by genre", fontsize=16, color='white')
+    ax.set_xlabel("Genre", fontsize=12, color='white')
+    ax.set_ylabel("Games", fontsize=12, color='white')
+
+    ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    png_file_name = f"{os.path.splitext(file_name)[0]}_genre.png"
+    plt.savefig(png_file_name, bbox_inches='tight', dpi=300)
+    logging.info("'genre bar chart' completed.")
+    plt.show()
+
+# # # # # # # # # # # #
+#   AGE RATING        #
+# # # # # # # # # # # #
+
+# counts all the games by publisher
+def count_age_rating(df):
+    logging.info("'count age rating' started.")
+    count_age_rating = df["Age Rating"].value_counts()
+    count_age_rating.index.name = None
+    print("Games by age rating:")
+    print(count_age_rating.to_string(name=False))
+    logging.info("'count age rating' completed.")
+
+# highlight the most common payment method
+def most_common_age_rating(df):
+    logging.info("'most common age rating' started.")
+    most_common_age_rating = df["Age Rating"].mode().iloc[0]
+    print(f"The most common age rating is: {most_common_age_rating}")
+    logging.info("'most common age rating' completed.")
+
+# highlight the least common payment method
+def least_common_age_rating(df):
+    logging.info("'least common age rating' started.")
+    least_common_age_rating = min(df["Age Rating"].unique(), key=df["Age Rating"].tolist().count)
+    print(f"The least common age rating is: {least_common_age_rating}")
+    logging.info("'least common age rating' completed.")
+
+# age rating bar chart
+def age_rating_bar_chart(df, file_name):
+    logging.info("'age rating bar chart' started.")
+
+    age_rating_count = df['Age Rating'].value_counts()
+
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    bar_colors = sns.color_palette("viridis", len(age_rating_count))
+    bars = ax.bar(age_rating_count.index, age_rating_count, color=bar_colors)
+
+    ax.set_title("Games by age rating", fontsize=16, color='white')
+    ax.set_xlabel("Age Rating", fontsize=12, color='white')
+    ax.set_ylabel("Games", fontsize=12, color='white')
+
+    ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.3)
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, f"{yval:.0f}", ha='center', va='bottom', color='white', fontsize=8)
+
+    png_file_name = f"{os.path.splitext(file_name)[0]}_age_rating.png"
+    plt.savefig(png_file_name, bbox_inches='tight', dpi=300)
+    logging.info("'age rating bar chart' completed.")
+    plt.show()
+
+# # # # # # # # # # # #
+#   END               #
+# # # # # # # # # # # #
---
+++ video_games_access.py
@@ -0,0 +1,92 @@
+import video_games as vg
+
+import pandas as pd
+import sys
+from io import StringIO
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+
+def run_video_games_access(file_name):
+    try:
+        log_file_path = vg.get_file_path(file_name, "log")
+        txt_file_path = vg.get_file_path(file_name, "txt")
+        png_file_path = vg.get_file_path(file_name, "png")
+        # xlsx_file_path = vg.get_file_path(file_name, "xlsx")
+        csv_file_path = vg.get_file_path(file_name, "csv")
+
+        vg.setup_logging(log_file_path)
+        logging.info("'video games access' script started.")
+
+        vg.check_csv_file(file_name)
+        df, output_file, output_buffer = vg.start_process_csv_file(file_name)
+        vg.columns_validation(df)
+
+        # vg.print_info(df)
+        # vg.print_description(df)
+        # vg.print_first_row(df)
+
+        vg.remove_columns(df)
+        vg.rename_column(df)
+        # vg.remove_rows(df)
+        vg.remove_duplicates(df)
+        vg.reset_index(df)
+        vg.check_missing_values(df)
+        vg.clean_age_rating(df)
+
+        cleaned_csv = vg.get_file_path(f"{os.path.splitext(file_name)[0]}_cleaned", "csv")
+        df.to_csv(cleaned_csv, index=False)
+
+        vg.count_developer(df)
+        vg.most_common_developer(df)
+        vg.least_common_developer(df)
+
+        vg.count_publisher(df)
+        vg.most_common_publisher(df)
+        vg.least_common_publisher(df)
+
+        vg.count_genre(df)
+        vg.most_common_genre(df)
+        vg.least_common_genre(df)
+
+        vg.count_age_rating(df)
+        vg.most_common_age_rating(df)
+        vg.least_common_age_rating(df)
+
+        vg.release_date_bar_chart(df, file_name)
+        vg.developer_bar_chart(df, file_name)
+        vg.publisher_bar_chart(df, file_name)
+        vg.genre_bar_chart(df, file_name)
+        vg.age_rating_bar_chart(df, file_name)
+
+        cleaned_df = df.copy()
+        return cleaned_df
+
+    except Exception as e:
+        logging.error(f"An unexpected {type(e).__name__} error occurred: {e}")
+        sys.exit(1)
+
+    finally:
+        vg.end_process_csv_file(output_file, output_buffer)
+
+    logging.info("'video games access' script completed.")
+
+    log_file_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.basename(log_file_path))
+    os.replace(log_file_path, log_file_destination)
+
+    txt_file_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.basename(txt_file_path))
+    os.replace(txt_file_path, txt_file_destination)
+
+    png_file_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.basename(png_file_path))
+    os.replace(png_file_path, png_file_destination)
+
+    # xlsx_file_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.basename(xlsx_file_path))
+    # os.replace(xlsx_file_path, xlsx_file_destination)
+
+    csv_file_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.basename(csv_file_path))
+    os.replace(csv_file_path, csv_file_destination)
+
+if __name__ == "__main__":
+    file_name = input("Enter the file name (including extension): ")
+    run_video_games_access(file_name)
